fd2734a233b644007883b5fbaff764dc18bc8c61
Shallen-99 <shallenburks@gmail.com>
2025-12-02 20:33:38 -0500

    added getTrendingMovies and displayMovies functions

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-// JavaScript Test
-alert("Heyyyy")
+const API_KEY = "d1fdb22551613717a622bc5adbf2893a"
+const BASE_URL = "https://api.themoviedb.org/3"
+
+async function getTrendingMovies() {
+    const res = await fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`)
+    const data = await res.json()
+    displayMovies(data.results)
+}
+
+function displayMovies(movies) {
+    const movieContainer = document.getElementById("movieContainer")
+    movieContainer.innerHTML = ""
+
+    movies.forEach(movie => {
+        const div = document.createElement("div")
+        div.classList.add("movie")
+        div.innerHTML = `
+        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${moive.title}">
+        <h3>${movie.title}<h3>
+        `
+        div.addEventListener("click", () => openMovieDetails(movie.id))
+        movieContainer.appendChild(div)
+    })
+}
